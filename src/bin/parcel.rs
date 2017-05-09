3dc09c397b67a481a4a8cbb3ed3ff50ec913200a
Luca Pizzamiglio <luca.pizzamiglio@gmail.com>
2017-05-09 11:38:15 +0200

    Small refactoring in the -R handling

--- src/bin/parcel.rs
+++ src/bin/parcel.rs
@@ -16,46 +16,30 @@ fn main() {
 	let matches = App::from_yaml(yaml).get_matches();
 
 	if matches.is_present("repositories") {
+		let repodirs = [ "/etc/pkg", "/usr/local/etc/pkg/repos" ];
 		let mut repos: Vec<Repo> = Vec::new();
-		for repofile in WalkDir::new("/etc/pkg")
-			.into_iter()
-			.filter_map( |e| e.ok())
-			.filter( |e| e.file_type().is_file())
-			.filter( |e| e.path().extension().unwrap_or( std::ffi::OsStr::new("")) == "conf" ) {
-			let f = match OpenOptions::new()
-				.read( true )
-				.write( false )
-				.create( false )
-				.open( repofile.path() ) {
-				Err(_) => continue,
-				Ok(f) => f,
-			};
+		for repodir in repodirs.into_iter() {
+			for repofile in WalkDir::new(repodir)
+				.into_iter()
+				.filter_map( |e| e.ok())
+				.filter( |e| e.file_type().is_file())
+				.filter( |e| e.path().extension().unwrap_or( std::ffi::OsStr::new("")) == "conf" ) {
+				let f = match OpenOptions::new()
+					.read( true )
+					.write( false )
+					.create( false )
+					.open( repofile.path() ) {
+					Err(_) => continue,
+					Ok(f) => f,
+				};
 
-			let buf_reader = &mut BufReader::new( f );
-			for v in multi_parse_file( buf_reader ) {
-				merge_repo( &mut repos, v );
+				let buf_reader = &mut BufReader::new( f );
+				for v in multi_parse_file( buf_reader ) {
+					merge_repo( &mut repos, v );
+				}
 			}
 		}
-		for repofile in WalkDir::new("/usr/local/etc/pkg/repos")
-			.into_iter()
-			.filter_map( |e| e.ok())
-			.filter( |e| e.file_type().is_file())
-			.filter( |e| e.path().extension().unwrap_or( std::ffi::OsStr::new("")) == "conf" ) {
-			let f = match OpenOptions::new()
-				.read( true )
-				.write( false )
-				.create( false )
-				.open( repofile.path() ) {
-				Err(_) => continue,
-				Ok(f) => f,
-			};
-
-			let buf_reader = &mut BufReader::new( f );
-			for v in multi_parse_file( buf_reader ) {
-				merge_repo( &mut repos, v );
-			}
-		}
-		println!("{:?}", repos);
+//		println!("{:?}", repos);
 		println!("Enabled repos:");
 		for r in repos.iter().filter(|x| x.enabled) {
 			println!("repo: {}", r.name);
